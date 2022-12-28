9bd332d779012ee629bac1ec0902d3d5f695aef9
sebastien <sebastien4shopping@gmail.com>
2022-12-28 21:25:47 +0100

    - Publish version 1.0 updated

--- src/com/gpxmanager/MyGPXManager.java
+++ src/com/gpxmanager/MyGPXManager.java
@@ -15,7 +15,6 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -54,7 +53,7 @@ import static com.gpxmanager.Utils.getLabel;
 
 public final class MyGPXManager extends JFrame {
 
-    public static final String INTERNAL_VERSION = "3.1";
+    public static final String INTERNAL_VERSION = "3.2";
     public static final String VERSION = "1";
     private static final MyAutoHideLabel INFO_LABEL = new MyAutoHideLabel();
     private final JMenuItem saveFile;
@@ -130,10 +129,14 @@ public final class MyGPXManager extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new MigLayout("", "grow", "[][grow][]"));
         add(panel, BorderLayout.CENTER);
-        // TODO
-        JLabel update = new JLabel("update");
-        update.setVisible(false);
+        MyAutoHideLabel update = new MyAutoHideLabel();
         panel.add(update, "gapleft 20, gaptop 10, hidemode 1, wrap");
+        if (MyGPXManagerServer.getInstance().hasAvailableUpdate(MyGPXManagerServer.getLocalVersion())) {
+            update.setVisible(true);
+            update.setText(MessageFormat.format(getLabel("Main.UpdateAvailable"), MyGPXManagerServer.getInstance().getAvailableVersion(), VERSION + "-" + INTERNAL_VERSION), true, 30000, false);
+        } else {
+            update.setVisible(false);
+        }
         panel.add(myTabbedPane = new MyTabbedPane(), "grow, hidemode 3, wrap");
         myTabbedPane.setVisible(false);
 
--- src/com/gpxmanager/launcher/MyGPXManagerServer.java
+++ src/com/gpxmanager/launcher/MyGPXManagerServer.java
@@ -2,12 +2,20 @@ package com.gpxmanager.launcher;
 
 import com.sebastienduche.Server;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static com.gpxmanager.MyGPXManager.INTERNAL_VERSION;
+
 public class MyGPXManagerServer extends Server {
 
     private static final MyGPXManagerServer INSTANCE = new MyGPXManagerServer();
 
     private MyGPXManagerServer() {
-        super("https://github.com/sebastienduche/mygpxmanager/raw/master/Build/", "MyGPXManagerVersion.txt", "MyGPXManager", "MyGPXManagerDebug");
+        super("https://github.com/sebastienduche/gpxmanager/raw/master/Build/", "MyGPXManagerVersion.txt", "MyGPXManager", "MyGPXManagerDebug");
     }
 
     public static MyGPXManagerServer getInstance() {
@@ -16,5 +24,32 @@ public class MyGPXManagerServer extends Server {
 
     public static void Debug(String sText) {
         getInstance().debug(sText);
+    }
+
+    public static String getLocalVersion() {
+        // In directory bin
+        File versionFile = new File(getInstance().getVersionFileName());
+        if (versionFile.exists()) {
+            try (var bufferReader = new BufferedReader(new FileReader(versionFile))) {
+                return bufferReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            setLocalVersion(INTERNAL_VERSION);
+            return INTERNAL_VERSION;
+        }
+        return "";
+    }
+
+    public static void setLocalVersion(String version) {
+        File f = new File(getInstance().getVersionFileName());
+        try (var writer = new FileWriter(f)) {
+            writer.write(version);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
