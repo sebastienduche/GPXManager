050bf32fb84916f2ea667d70c9e2f17f05b825ef
sebastien <sebastien4shopping@gmail.com>
2023-01-17 18:31:40 +0100

    - Publish version 2.0 updated - New GitHubServer-2.4.jar

--- src/com/gpxmanager/MyGPXManager.java
+++ src/com/gpxmanager/MyGPXManager.java
@@ -53,7 +53,7 @@ import java.util.prefs.Preferences;
 import static com.gpxmanager.Utils.getLabel;
 
 public final class MyGPXManager extends JFrame {
-    public static final String INTERNAL_VERSION = "5.0";
+    public static final String INTERNAL_VERSION = "5.1";
     public static final String VERSION = "2.0";
     private static final MyAutoHideLabel INFO_LABEL = new MyAutoHideLabel();
     private final JMenuItem saveFile;
