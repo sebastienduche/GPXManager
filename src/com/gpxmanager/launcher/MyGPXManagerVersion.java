c000954d6ca2db8c0907c5ae633e8824474d6f53
sebastien <sebastien4shopping@gmail.com>
2023-01-18 17:09:53 +0100

    - Publish version 2.0 updated - New GitHubServer-2.4.jar

--- src/com/gpxmanager/launcher/MyGPXManagerVersion.java
+++ src/com/gpxmanager/launcher/MyGPXManagerVersion.java
@@ -8,30 +8,31 @@ import java.io.IOException;
 
 public class MyGPXManagerVersion {
 
-	public static String getLocalVersion() {
-		// In directory bin
-		File versionFile = new File("MyGPXManagerVersion.txt");
-		if (versionFile.exists()) {
-    		try(var bufferReader = new BufferedReader(new FileReader(versionFile)))
-    		{
-    			return bufferReader.readLine();
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    		}
-		} else {
-			setLocalVersion("1.0");
-		}
-		return "";
-	}
-	
-	public static void setLocalVersion(String version) {
-		File f = new File("MyGPXManagerVersion.txt");
-		try (var writer = new FileWriter(f)) {
-			writer.write(version);
-			writer.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    private static final String VERSION_FILE = "MyGPXManagerVersion.txt";
+
+    public static String getLocalVersion() {
+        // In directory bin
+        File versionFile = new File(VERSION_FILE);
+        if (versionFile.exists()) {
+            try (var bufferReader = new BufferedReader(new FileReader(versionFile))) {
+                return bufferReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            setLocalVersion("1.0");
+        }
+        return "";
+    }
+
+    public static void setLocalVersion(String version) {
+        File f = new File(VERSION_FILE);
+        try (var writer = new FileWriter(f)) {
+            writer.write(version);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
