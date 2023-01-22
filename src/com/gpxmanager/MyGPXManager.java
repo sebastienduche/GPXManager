615855e0914be516ca301f69c617085ec918d8f1
sebastien <sebastien4shopping@gmail.com>
2023-01-22 18:31:29 +0100

    - Publish version 3.0

--- src/com/gpxmanager/MyGPXManager.java
+++ src/com/gpxmanager/MyGPXManager.java
@@ -53,8 +53,8 @@ import java.util.prefs.Preferences;
 import static com.gpxmanager.Utils.getLabel;
 
 public final class MyGPXManager extends JFrame {
-    public static final String INTERNAL_VERSION = "5.2";
-    public static final String VERSION = "2.0";
+    public static final String INTERNAL_VERSION = "5.7";
+    public static final String VERSION = "3.0";
     private static final MyAutoHideLabel INFO_LABEL = new MyAutoHideLabel();
     private final JMenuItem saveFile;
     private final JMenuItem saveAsFile;
