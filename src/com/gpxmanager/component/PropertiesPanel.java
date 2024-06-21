0494a0674a6436a9fedbed7d98dad78551de5080
sebastien <sebastien4shopping@gmail.com>
2024-06-21 11:16:59 +0200

    fix indentation

--- src/com/gpxmanager/component/PropertiesPanel.java
+++ src/com/gpxmanager/component/PropertiesPanel.java
@@ -22,128 +22,128 @@ import static com.gpxmanager.Utils.roundValue;
 
 public class PropertiesPanel extends JPanel {
 
-    private final JModifyTextField metadataName = new JModifyTextField();
-    private final JModifyTextField metadataDescription = new JModifyTextField();
-    private final JModifyTextField metadataAuthor = new JModifyTextField();
-    private final JModifyTextField metadataKeywords = new JModifyTextField();
-    private final JModifyFormattedTextField metadataTime = new JModifyFormattedTextField(TIMESTAMP, true);
+  private final JModifyTextField metadataName = new JModifyTextField();
+  private final JModifyTextField metadataDescription = new JModifyTextField();
+  private final JModifyTextField metadataAuthor = new JModifyTextField();
+  private final JModifyTextField metadataKeywords = new JModifyTextField();
+  private final JModifyFormattedTextField metadataTime = new JModifyFormattedTextField(TIMESTAMP, true);
 
-    public PropertiesPanel(GPX gpx) {
-        setLayout(new MigLayout("", "[][grow]10px[][grow]10px[][grow]", "grow"));
-        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), getLabel("properties.title")));
-        if (gpx != null) {
-            add(new JLabel(getLabel("properties.trackCount")));
-            add(new JBoldLabel(getTrackCount(gpx)));
-            add(new JLabel(getLabel("properties.routeCount")));
-            add(new JBoldLabel(getRouteCount(gpx)));
-            add(new JLabel(getLabel("properties.waypointCount")));
-            add(new JBoldLabel(getWaypointCount(gpx)), "wrap");
-            add(new JLabel(getLabel("properties.totalDistance")));
-            double totalDistance = 0;
-            for (Track track : gpx.getTracks()) {
-                totalDistance += getTrackDistance(track.getTrackPoints());
-            }
-            add(new JBoldLabel(roundValue(totalDistance) + " " + getLabel("km")));
-            add(new JLabel(getLabel("properties.totalTime")));
-            MyTime totalTime = new MyTime();
-            for (Track track : gpx.getTracks()) {
-                totalTime = MyTime.add(totalTime, getTrackTime(track.getTrackPoints()));
-            }
-            if (totalTime != null && totalTime.time() != 0) {
-                add(new JBoldLabel(totalTime.toString()), "wrap");
-            } else {
-                add(new JLabel(), "wrap");
-            }
-        }
-
-        Metadata metadata = gpx != null ? gpx.getMetadata() : new Metadata();
-        if (metadata != null) {
-            add(new JLabel(getLabel("properties.name")));
-            metadataName.setText(metadata.getName());
-            add(metadataName, "growx");
-            add(new JLabel(getLabel("properties.description")));
-            metadataDescription.setText(metadata.getDescription());
-            add(metadataDescription, "growx, span 4, wrap");
-
-            add(new JLabel(getLabel("properties.author")));
-            metadataAuthor.setText(metadata.getAuthor());
-            add(metadataAuthor, "growx");
-            add(new JLabel(getLabel("properties.keywords")));
-            metadataKeywords.setText(metadata.getKeywords());
-            add(metadataKeywords, "growx, span 4, wrap");
-            add(new JLabel(getLabel("properties.time")));
-            metadataTime.setValue(metadata.getTime());
-            add(metadataTime, "growx, wrap");
-
-            metadataName.setModified(false);
-            metadataDescription.setModified(false);
-            metadataAuthor.setModified(false);
-            metadataKeywords.setModified(false);
-            metadataTime.setModified(false);
-        }
+  public PropertiesPanel(GPX gpx) {
+    setLayout(new MigLayout("", "[][grow]10px[][grow]10px[][grow]", "grow"));
+    setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), getLabel("properties.title")));
+    if (gpx != null) {
+      add(new JLabel(getLabel("properties.trackCount")));
+      add(new JBoldLabel(getTrackCount(gpx)));
+      add(new JLabel(getLabel("properties.routeCount")));
+      add(new JBoldLabel(getRouteCount(gpx)));
+      add(new JLabel(getLabel("properties.waypointCount")));
+      add(new JBoldLabel(getWaypointCount(gpx)), "wrap");
+      add(new JLabel(getLabel("properties.totalDistance")));
+      double totalDistance = 0;
+      for (Track track : gpx.getTracks()) {
+        totalDistance += getTrackDistance(track.getTrackPoints());
+      }
+      add(new JBoldLabel(roundValue(totalDistance) + " " + getLabel("km")));
+      add(new JLabel(getLabel("properties.totalTime")));
+      MyTime totalTime = new MyTime();
+      for (Track track : gpx.getTracks()) {
+        totalTime = MyTime.add(totalTime, getTrackTime(track.getTrackPoints()));
+      }
+      if (totalTime != null && totalTime.time() != 0) {
+        add(new JBoldLabel(totalTime.toString()), "wrap");
+      } else {
+        add(new JLabel(), "wrap");
+      }
     }
 
-    private String getTrackCount(GPX gpx) {
-        return gpx.getTracks() == null ? "0" : Integer.toString(gpx.getTracks().size());
-    }
+    Metadata metadata = gpx != null ? gpx.getMetadata() : new Metadata();
+    if (metadata != null) {
+      add(new JLabel(getLabel("properties.name")));
+      metadataName.setText(metadata.getName());
+      add(metadataName, "growx");
+      add(new JLabel(getLabel("properties.description")));
+      metadataDescription.setText(metadata.getDescription());
+      add(metadataDescription, "growx, span 4, wrap");
 
-    private String getRouteCount(GPX gpx) {
-        return gpx.getRoutes() == null ? "0" : Integer.toString(gpx.getRoutes().size());
-    }
+      add(new JLabel(getLabel("properties.author")));
+      metadataAuthor.setText(metadata.getAuthor());
+      add(metadataAuthor, "growx");
+      add(new JLabel(getLabel("properties.keywords")));
+      metadataKeywords.setText(metadata.getKeywords());
+      add(metadataKeywords, "growx, span 4, wrap");
+      add(new JLabel(getLabel("properties.time")));
+      metadataTime.setValue(metadata.getTime());
+      add(metadataTime, "growx, wrap");
 
-    private String getWaypointCount(GPX gpx) {
-        return gpx.getWaypoints() == null ? "0" : Integer.toString(gpx.getWaypoints().size());
+      metadataName.setModified(false);
+      metadataDescription.setModified(false);
+      metadataAuthor.setModified(false);
+      metadataKeywords.setModified(false);
+      metadataTime.setModified(false);
     }
+  }
 
-    public boolean save(Metadata metadata) throws ParseException {
-        if (metadataName.isModified()) {
-            metadata.setName(metadataName.getText());
-        }
-        if (metadataDescription.isModified()) {
-            metadata.setDescription(metadataDescription.getText());
-        }
-        if (metadataAuthor.isModified()) {
-            metadata.setAuthor(metadataAuthor.getText());
-        }
-        if (metadataKeywords.isModified()) {
-            metadata.setKeywords(metadataKeywords.getText());
-        }
-        if (metadataTime.isModified() && metadataTime.isValid()) {
-            if (metadataTime.getText() == null || metadataTime.getText().isBlank()) {
-                metadata.setTime(null);
-            } else {
-                try {
-                    metadata.setTime(TIMESTAMP.parse(metadataTime.getText()));
-                } catch (ParseException e) {
-                    JOptionPane.showMessageDialog(this, getLabel("error.timestamp"), getLabel("error.title"), JOptionPane.ERROR_MESSAGE);
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+  private String getTrackCount(GPX gpx) {
+    return gpx.getTracks() == null ? "0" : Integer.toString(gpx.getTracks().size());
+  }
 
-    public void load(Metadata metadata) {
-        if (metadata == null) {
-            return;
-        }
-        metadataName.setText(metadata.getName());
-        metadataDescription.setText(metadata.getDescription());
-        metadataAuthor.setText(metadata.getAuthor());
-        metadataKeywords.setText(metadata.getKeywords());
-        metadataTime.setValue(metadata.getTime());
-        metadataName.setModified(false);
-        metadataDescription.setModified(false);
-        metadataAuthor.setModified(false);
-        metadataKeywords.setModified(false);
-        metadataTime.setModified(false);
-    }
+  private String getRouteCount(GPX gpx) {
+    return gpx.getRoutes() == null ? "0" : Integer.toString(gpx.getRoutes().size());
+  }
 
-    public boolean isModified() {
-        return metadataName.isModified() ||
-                metadataDescription.isModified() ||
-                metadataAuthor.isModified() ||
-                metadataKeywords.isModified() ||
-                metadataTime.isModified();
+  private String getWaypointCount(GPX gpx) {
+    return gpx.getWaypoints() == null ? "0" : Integer.toString(gpx.getWaypoints().size());
+  }
+
+  public boolean save(Metadata metadata) throws ParseException {
+    if (metadataName.isModified()) {
+      metadata.setName(metadataName.getText());
     }
+    if (metadataDescription.isModified()) {
+      metadata.setDescription(metadataDescription.getText());
+    }
+    if (metadataAuthor.isModified()) {
+      metadata.setAuthor(metadataAuthor.getText());
+    }
+    if (metadataKeywords.isModified()) {
+      metadata.setKeywords(metadataKeywords.getText());
+    }
+    if (metadataTime.isModified() && metadataTime.isValid()) {
+      if (metadataTime.getText() == null || metadataTime.getText().isBlank()) {
+        metadata.setTime(null);
+      } else {
+        try {
+          metadata.setTime(TIMESTAMP.parse(metadataTime.getText()));
+        } catch (ParseException e) {
+          JOptionPane.showMessageDialog(this, getLabel("error.timestamp"), getLabel("error.title"), JOptionPane.ERROR_MESSAGE);
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public void load(Metadata metadata) {
+    if (metadata == null) {
+      return;
+    }
+    metadataName.setText(metadata.getName());
+    metadataDescription.setText(metadata.getDescription());
+    metadataAuthor.setText(metadata.getAuthor());
+    metadataKeywords.setText(metadata.getKeywords());
+    metadataTime.setValue(metadata.getTime());
+    metadataName.setModified(false);
+    metadataDescription.setModified(false);
+    metadataAuthor.setModified(false);
+    metadataKeywords.setModified(false);
+    metadataTime.setModified(false);
+  }
+
+  public boolean isModified() {
+    return metadataName.isModified() ||
+        metadataDescription.isModified() ||
+        metadataAuthor.isModified() ||
+        metadataKeywords.isModified() ||
+        metadataTime.isModified();
+  }
 }
